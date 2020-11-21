d4ef683737b37605c0601ad224cf3438f9e62d71
Pijus Eigirdas <pijus@pop-os.localdomain>
2020-11-21 22:41:46 +0200

    remove bitstore as a temporary, write output directy to out variable

--- floaty/src/main.cpp
+++ floaty/src/main.cpp
@@ -1,5 +1,4 @@
 // IMPLEMENT ADDITIONAL STUFF FROM: https://www.youtube.com/watch?v=wbxSTxhTmrs 
-
 int POWERBASEX(const int exp, int base, int res = 1) {
     for (int i = 0; i < exp; ++i) res *= base;
     return res;
@@ -26,8 +25,7 @@ float Convert(const char x[]) {
     int exp_pow = 7;
     int mantissa_pow = 1;
 
-    int sign = (x[0] == '0') ? 1 : -1;
-    ++x;  // we got sign skip to next token
+    int sign = (x[0] == '0') ? 1 : -1; ++x; // we got sign skip to next token
     int exponent = -127;
     float mantissa = 1.0;
 
@@ -49,9 +47,9 @@ void Reconvert(const char x[], char out[], int N) {
     if (*x == '-') {
         out[0] = '1';
         ++x;
-    } else
-        out[0] = '0';
-
+    } else{
+        out[0] = '0'; 
+    }
     // Convert natural part from chars to an integer
     int natural_part = 0;
     // TODO: figure out what to do for inputs ("10000") with no .
@@ -66,55 +64,38 @@ void Reconvert(const char x[], char out[], int N) {
         numerator = numerator * 10 + (*x - '0');
     denominator = POWBASE10(denominator);
 
-    // convert to fixed representation
-    constexpr int M = 24;  // 23 mantissa for 32 float, we discard last 1 and
-                           // add when back to float "(1 + mantissa) ....
-    char bitstore[M] = {'0', '0', '0', '0', '0', '0', '0', '0',
-                        '0', '0', '0', '0', '0', '0', '0', '0',
-                        '0', '0', '0', '0', '0', '0', '0', '0'};
 
     int idx = 0; 
     bool started = false;
     for(int i = 31; i >= 0; --i){
-        if(started) bitstore[idx++] = (natural_part & (1 << i)) ? '1' : '0'; 
-        else if (!started && (natural_part & (1 << i))) {started = true; bitstore[idx] = '1'; ++idx;} 
-    }
-
-
-
-
-
-
-
+        if(started) out[9 + idx++] = (natural_part & (1 << i)) ? '1' : '0'; 
+        else if (!started && (natural_part & (1 << i))) started = true;
+    } 
 
     // get the exponent part (8 bits)
-    int exp = (idx - 1) + 127;
+    // TODO: make this with binary stuff and what not
+    int exp = idx + 127;
     for (int i = 8; i >= 1; --i, exp /= 2) {
         out[i] = (exp % 2) ? '1' : '0';
-    }
-
+    } 
     // convert the decimal part
-    for (; idx < M; ++idx) {
+    // TODO: we can do early cut off here if we want (if like its 0 then it cant get bigger so all esle are 0
+    for (; (9+idx) < 32; ++idx) {
         numerator *= 2;
         if (numerator >= denominator) {
-            bitstore[idx] = '1';
+            out[9 + idx] = '1';
             numerator -= denominator;
         }
-    }
-
+        else out[9 + idx] = '0';
+    } 
     // ROUNDING: https://www.youtube.com/watch?v=wbxSTxhTmrs
     if ((numerator * 2) > denominator) {
         --idx; 
-
-        while (bitstore[idx] != '0') {
-            bitstore[idx] = '0';
+        while (out[9 + idx] != '0') {
+            out[9 + idx] = '0';
             --idx;
         }
-        bitstore[idx] = '1';
-    }
-
-    for (int i = 9; i < 32; ++i) {
-        out[i] = bitstore[i - 8];
-    }
+        out[9 + idx] = '1';
+    } 
     out[32] = '\0';
 }
